897097d71ba0cc1e03c27c6b4a0938fad09307b5
Leon B <leon.borlinghaus@gmail.com>
2018-02-07 22:27:35 +0100

    update models, add no image selection

--- src/app/shared/components/image-card/image-card.component.ts
+++ src/app/shared/components/image-card/image-card.component.ts
@@ -13,6 +13,7 @@ export class ImageCardComponent implements OnInit {
   @Input() dir;
   @Input() selected;
   @Output() onClick = new EventEmitter();
+  @Input() index = 0;
   
   public isLoading = true;
   public baseUrl = environment.serverUrl;
@@ -29,6 +30,14 @@ export class ImageCardComponent implements OnInit {
   hideLoader(): void {
     console.log("image loaded");
     this.isLoading = false;
+  }
+
+  getImageLink(): string {
+    if( this.index == 0 && this.dir.allowNoImage == true){
+      return this.baseUrl + "/images/no-image.png";
+    }else{
+      return this.baseUrl + '/images/bimages/' + this.dir.directoryName + '/' + this.image.fileName;
+    }
   }
 
 }
